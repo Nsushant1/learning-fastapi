b07974b28727e552b0afef9f4c30e7ddf635e423
Sushant Neupane <nsushaant72@gmail.com>
2026-05-26 21:24:56 +0545

    Add .gitignore file and clean up issues.py

--- app/routes/issues.py
+++ app/routes/issues.py
@@ -4,8 +4,6 @@ from app.schema import IssueCreate, IssueUpdate, IssueOut, IssueStatus, IssuePri
 from app.storage import load_data, save_data 
 
 
-
-
 router = APIRouter(prefix="/api/v1/issues", tags=["issues"])
 
 @router.get("/", response_model=list[IssueOut])
